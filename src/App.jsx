9f6ee21d73aa7b0b148a2ed98ace25cac605d304
Roque Pulido <roque.pulido@outlook.com>
2023-01-05 12:15:27 -0500

    creacion de archivo NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import "./css/App.css";
+
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <NavBar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
