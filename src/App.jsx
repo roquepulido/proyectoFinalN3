3a1f8c2968bf6b1d7594a49fe06a3e89db6c9aac
RoqueP <roque.pulido@outlook.com>
2023-01-06 02:42:47 -0500

    Creacion y maquetacion de Menu despegable

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./css/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle"
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App d-flex flex-column flex-sm-row">
-      <div className="left__panel col col-sm-4 vh-100">
+      <div className="left__panel col col-sm-5 vh-100">
         <NavBar />
         <div className="imgClima">
           <img src="./img/Clear.png" alt="" />
         </div>
       </div>
-      <div className="right__panel col col-sm-8 vh-100">
+      <div className="right__panel col col-sm vh-100">
         <p style={{ color: "white" }}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
           nobis provident modi, quam suscipit sit corrupti repudiandae. Sit,
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,61 +1,55 @@
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Offcanvas from "react-bootstrap/Offcanvas";
-
 function NavBar() {
   return (
     <>
-      {[false].map((expand) => (
-        <Navbar key={expand} expand={expand} className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="start"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">TEST</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+      <div className="container__buttons d-flex justify-content-between p-3">
+        <button
+          class="button__search"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#searchMenu"
+          aria-controls="searchMenu"
+        >
+          <span className="texto__claro">Seach for places</span>
+        </button>
+        <button className="button__localitation">
+          <span class="material-symbols-outlined">my_location</span>
+        </button>
+      </div>
+
+      <div
+        class="offcanvas offcanvas-start offcanvas__search"
+        data-bs-scroll="true"
+        tabindex="-1"
+        id="searchMenu"
+        aria-labelledby="searchMenuLabel"
+      >
+        <div class="offcanvas-header d-flex flex-row-reverse">
+          <button
+            type="button"
+            class="btn-close btn-close-white"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body h-100">
+          
+            <div className="row g-0 form__search">
+              <input
+                className="col-8 me-2"
+                type="search"
+                placeholder="search location"
+                aria-label="Search"
+              />
+              <button className="col" type="">
+                Search
+              </button>
+            </div>
+          
+          <button className="button__pais texto__claro">M&eacute;xico</button>
+          <button className="button__pais texto__claro">Lima</button>
+          <button className="button__pais texto__claro">Guatemala</button>
+        </div>
+      </div>
     </>
   );
 }
