5c7f4b5d95951d94ea9cc6eb346a5dc665e5e861
Roque Pulido <roque.pulido@outlook.com>
2023-01-05 21:54:04 -0500

    inicio de maquetacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./css/App.css";
-
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div className="App d-flex flex-column flex-sm-row">
+      <div className="left__panel col col-sm-4 vh-100">
+        <NavBar />
+        <div className="imgClima">
+          <img src="./img/Clear.png" alt="" />
+        </div>
+      </div>
+      <div className="right__panel col col-sm-8 vh-100">
+        <p style={{ color: "white" }}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+          nobis provident modi, quam suscipit sit corrupti repudiandae. Sit,
+          perspiciatis!
+        </p>
+      </div>
     </div>
   );
 }
 
 export default App;
+459;
+981;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 function NavBar() {
   return (
     <>
       {[false].map((expand) => (
-        <Navbar key={expand} bg="light" expand={expand} className="mb-3 col-md-6">
+        <Navbar key={expand} expand={expand} className="mb-3">
           <Container fluid>
             <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
