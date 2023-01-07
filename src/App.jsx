1d3ef0d07f304304739ad7751e0f3197af7f41ec
RoqueP <roque.pulido@outlook.com>
2023-01-06 23:51:57 -0500

    maquetacion de five Cards y Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import SearchMenu from "./components/SearchMenu";
 import PrincipalWidget from "./components/PrincipalWidget";
+import ForecastFiveDay from "./components/ForecastFiveDay"
 
 function App() {
   return (
@@ -12,19 +13,8 @@ function App() {
         <SearchMenu />
         <PrincipalWidget />
       </div>
-      <div className="right__panel col col-sm vh-100">
-        <p style={{ color: "white" }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos culpa
-          quos molestias eum numquam a sit. Nulla rem eaque error excepturi
-          dolore sit assumenda maxime repellat natus. Eos blanditiis possimus
-          aliquam tempora autem rem velit vitae alias est nesciunt ab magni,
-          ullam soluta dolor officiis, excepturi maxime nemo temporibus
-          accusamus libero illo. Pariatur vitae quae numquam dolore ducimus iure
-          aliquam nam vero itaque beatae in harum, eius quos inventore atque
-          nisi animi id minima sunt ad ullam nemo. Delectus expedita quia
-          dolorem? Culpa quam minus totam dolorum, dicta reiciendis ea, rem
-          eligendi odit delectus, nisi mollitia sit. Cupiditate, ducimus quis.
-        </p>
+      <div className="right__panel col vh-100">
+        <ForecastFiveDay />
       </div>
     </div>
   );
