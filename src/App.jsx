29b15df7ab8f7255b147d999f31e8b7476368027
RoqueP <roque.pulido@outlook.com>
2023-01-07 01:45:56 -0500

    Ajustes CSS

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,13 @@ function App() {
         <PrincipalWidget />
       </div>
       <div className="right__panel col vh-100">
+        <div className="forecast_container border border-5 mx-auto">
         <ForecastFiveDay />
         <Hightlights />
-        <footer class="w-100 d-flex justify-content-center my-4">
-          created by <span class="fw-bold mx-1">roque.pulido</span> -
+
+        </div>
+        <footer className="w-100 d-flex justify-content-center my-4">
+          created by <span className="fw-bold mx-1">roque.pulido</span> -
           devChallenges.io
         </footer>
       </div>
