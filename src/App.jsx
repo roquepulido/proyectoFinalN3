f3b49de59fac2a336d323618891126e062354962
RoqueP <roque.pulido@outlook.com>
2023-01-07 01:01:58 -0500

    Maquetado aside derecho

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import SearchMenu from "./components/SearchMenu";
 import PrincipalWidget from "./components/PrincipalWidget";
-import ForecastFiveDay from "./components/ForecastFiveDay"
+import ForecastFiveDay from "./components/ForecastFiveDay";
+import Hightlights from "./components/Hightlights";
+
 
 function App() {
   return (
     <div className="App d-flex flex-column flex-sm-row">
       <div className="left__panel col d-flex flex-column justify-content-between overflow-hidden">
-      {/* div className="principal__widget  d-flex justify-content-evenly" */}
+        {/* div className="principal__widget  d-flex justify-content-evenly" */}
         <SearchMenu />
         <PrincipalWidget />
       </div>
       <div className="right__panel col vh-100">
         <ForecastFiveDay />
+        <Hightlights />
+        <footer class="w-100 d-flex justify-content-center my-4">
+          created by <span class="fw-bold mx-1">roque.pulido</span> -
+          devChallenges.io
+        </footer>
       </div>
     </div>
   );
